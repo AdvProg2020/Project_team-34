8f257bb739fd9dc76f342b0966002e2d8607175b
rpirayadi <roozbehpirayadi@yahoo.com>
2020-04-12 23:46:08 +0430

    Updated Class Supplier

--- src/main/java/account/Supplier.java
+++ src/main/java/account/Supplier.java
@@ -1,4 +1,28 @@
 package account;
 
-public class Supplier {
+import log.SupplierLog;
+
+public class Supplier extends Account{
+
+    private String nameOfCompany;
+    private SupplierLog supplierLog ;
+
+
+    public Supplier(String userName, String name, String familyName, String email, String phoneNumber, String password, int credit , String nameOfCompany) {
+        super(userName, name, familyName, email, phoneNumber, password, credit);
+        this.nameOfCompany = nameOfCompany;
+    }
+
+    public String getNameOfCompany() {
+        return nameOfCompany;
+    }
+
+    public void setNameOfCompany(String nameOfCompany) {
+        this.nameOfCompany = nameOfCompany;
+    }
+
+    @Override
+    public String getType() {
+        return "Supplier";
+    }
 }

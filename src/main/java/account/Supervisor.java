91484a53f2eb08866bc97ceae18542101f319636
rpirayadi <roozbehpirayadi@yahoo.com>
2020-04-12 23:46:57 +0430

    Updated Class Supervisor

--- src/main/java/account/Supervisor.java
+++ src/main/java/account/Supervisor.java
@@ -1,4 +1,13 @@
 package account;
 
-public class Supervisor {
+public class Supervisor extends Account {
+
+    public Supervisor(String userName, String name, String familyName, String email, String phoneNumber, String password, int credit) {
+        super(userName, name, familyName, email, phoneNumber, password, credit);
+    }
+
+    @Override
+    public String getType() {
+        return "Supervisor";
+    }
 }

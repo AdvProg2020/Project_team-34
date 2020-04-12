8c5b1d86dfc0e4b7d85bcc99480cd13aa5448f88
s0heil20 <soheil.mh2000@gmail.com>
2020-04-12 17:30:56 +0330

    add signatures of classes in discount and feedback

--- src/main/java/discount/CodedDiscount.java
+++ src/main/java/discount/CodedDiscount.java
@@ -1,4 +1,40 @@
 package discount;
 
-public class CodedDiscount {
+import account.Customer;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
+public class CodedDiscount extends Discount{
+    private String discountCode;
+    private int maxDiscountPercent;
+    private HashMap<Customer, Integer> customers;
+    private static ArrayList<CodedDiscount> codedDiscounts;
+
+    public CodedDiscount(Date start, Date end, int percent, String discountCode, int maxDiscountPercent) {
+        super(start, end, percent);
+        this.discountCode = discountCode;
+        this.maxDiscountPercent = maxDiscountPercent;
+    }
+
+    public void addCustomer(Customer customer){
+
+    }
+
+    public void addUsedCountForCustomer(){
+
+    }
+
+    public boolean canCustomerUseCode(Customer customer){
+        return true;
+    }
+
+    public static ArrayList<CodedDiscount> getCodedDiscounts() {
+        return codedDiscounts;
+    }
+
+    public void removeCodeFromList(CodedDiscount codedDiscount){
+
+    }
 }

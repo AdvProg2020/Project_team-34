d99aa201e3b118333d5270525f2002e474c0dfb0
rpirayadi <roozbehpirayadi@yahoo.com>
2020-04-14 00:18:21 +0430

    Merge branch 'master' of C:\Users\ASUS\IdeaProjects\Project_team-34 with conflicts.

--- src/main/java/request/ProductRequest.java
+++ src/main/java/request/ProductRequest.java
@@ -3,19 +3,30 @@ package request;
 
 import product.Product;
 
-public class ProductRequest extends Request{
+public class ProductRequest extends Request {
     private Product oldProduct;
     private Product newProduct;
+    private static int allCreatedProductRequestNum = 0;
 
-
-    @Override
-    public void doRequest() {
-        if (oldProduct != null)
-            oldProduct.removeProduct();
-        newProduct.addProduct();
+    public ProductRequest() {
+        super();
     }
 
 
+    @Override
+    public void doRequest(boolean acceptedOrNot) {
+        if (acceptedOrNot) {
+            if (oldProduct != null)
+                oldProduct.removeProduct();
+            newProduct.addProduct();
+        }
+        this.removeRequest();
+    }
+
+    @Override
+    public static String generateRequestId() {
+        return "T34PR" + String.format("%015d",allCreatedProductRequestNum  + 1);
+    }
 
     @Override
     public String toString() {

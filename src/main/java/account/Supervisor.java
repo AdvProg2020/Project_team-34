d5fd5d8b0f923e7786f249cf748285c53c553584
rpirayadi <roozbehpirayadi@yahoo.com>
2020-04-13 22:42:31 +0430

    modification

--- src/main/java/account/Supervisor.java
+++ src/main/java/account/Supervisor.java
@@ -5,6 +5,7 @@ package account;
  * @since 0.0.1
  * This class represents a supplier which is also an account
  */
+//
 public class Supervisor extends Account {
 
     public Supervisor(String userName, String name, String familyName, String email, String phoneNumber, String password, int credit) {

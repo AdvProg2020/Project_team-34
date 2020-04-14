c76261d8101e10d1813bc7e84d2c662d1b8158ce
Aryan Ahadinia <aryanahadinia24@gmail.com>
2020-04-14 21:13:22 +0430

    Add Menu

--- src/main/java/menu/profile/supervisorProfileMenu/ManageProductsMenu.java
+++ src/main/java/menu/profile/supervisorProfileMenu/ManageProductsMenu.java
@@ -1,4 +1,21 @@
 package menu.profile.supervisorProfileMenu;
 
-public class ManageProductsMenu {
+import menu.menuAbstract.Menu;
+
+public class ManageProductsMenu extends Menu {
+    public ManageProductsMenu(Menu parentMenu) {
+        super("Manage Products", parentMenu);
+
+        Menu RemoveProduct = new Menu("Remove Product", this) {
+            @Override
+            public void show() {
+            }
+
+            @Override
+            public void execute() {
+            }
+        };
+        menusIn.put("^remove \\w+$", RemoveProduct);
+        menuForShow.add("Remove");
+    }
 }

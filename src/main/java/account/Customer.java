1d304eb33b5dadce7d8d86ec002d8af0a5fc2f79
rpirayadi <roozbehpirayadi@yahoo.com>
2020-04-14 22:55:35 +0430

    Add cart

--- src/main/java/account/Customer.java
+++ src/main/java/account/Customer.java
@@ -1,5 +1,6 @@
 package account;
 
+import cart.Cart;
 import log.CustomerLog;
 
 /**
@@ -8,6 +9,7 @@ import log.CustomerLog;
  */
 public class Customer extends Account {
     private CustomerLog customerLog;
+    private Cart cart;
 
     public Customer(String userName, String name, String familyName, String email, String phoneNumber, String password, int credit) {
         super(userName, name, familyName, email, phoneNumber, password, credit);
--- src/main/java/product/Product.java
+++ src/main/java/product/Product.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 public class Product {
     private static ArrayList<Product> allProduct = new ArrayList<>();
     private static int allCreatedProductNum = 0 ;
+    private int  numberOfViews ;
 
     private String productId;
     private State productState;
@@ -39,10 +40,11 @@ public class Product {
         this.description = description;
         comments = new ArrayList<>();
         this.productState = State.PREPARING_TO_BUILD;
+        numberOfViews = 0;
     }
 
     public Product(String name, String nameOfCompany, int price, ArrayList<Supplier> listOfSuppliers,
-                   boolean isAvailable, Category category, String description, ArrayList<Comment> comments) {
+                   boolean isAvailable, Category category, String description, ArrayList<Comment> comments, int numberOfViews) {
         this.productState = State.PREPARING_TO_BUILD;
         this.name = name;
         this.nameOfCompany = nameOfCompany;
@@ -75,6 +77,10 @@ public class Product {
     }
 
     public static Product getProductById(String productId){return null;}
+
+    public int getNumberOfViews() {
+        return numberOfViews;
+    }
 
     public void removeProduct(){
         allProduct.remove(this);

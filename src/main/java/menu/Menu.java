4063efcbea18fc16aa9824095562f832362a1b00
Aryan Ahadinia <aryanahadinia24@gmail.com>
2020-04-14 18:03:39 +0430

    Change Menu

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -13,27 +13,24 @@ import java.util.regex.Pattern;
 
 public abstract class Menu {
     public static Scanner scanner = new Scanner(System.in);
-
-    private String menuName;
-    private Menu parentMenu;
-    protected ArrayList<String> commandForShow;
-    protected HashMap<String, Runnable> commandsIn;
     protected ArrayList<String> menuForShow;
     protected HashMap<String, Menu> menusIn;
-
-    private String userCode;
-
+    protected String userCode;
     protected String command;
+    private String menuName;
+    private Menu parentMenu;
 
     public Menu(String menuName, Menu parentMenu) {
         this.menuName = menuName;
         this.parentMenu = parentMenu;
     }
 
+    public static Matcher getMatcher(String command, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(command);
+    }
+
     public void show() {
-        for (String command : commandForShow) {
-            System.out.println(command);
-        }
         for (String menu : menuForShow) {
             System.out.println(menu);
         }
@@ -41,30 +38,13 @@ public abstract class Menu {
 
     public void execute() {
         command = scanner.nextLine();
-        Runnable nextCommand = null;
-        for (String commandRegex : commandsIn.keySet()) {
-            if (command.matches(commandRegex)) {
-                nextCommand = commandsIn.get(commandRegex);
-            }
-        }
         Menu nextMenu = null;
-        if (nextCommand == null) {
-            for (String menuRegex : menusIn.keySet()) {
-                if (command.matches(menuRegex)) {
-                    nextMenu = menusIn.get(menuRegex);
-                }
+        for (String menuRegex : menusIn.keySet()) {
+            if (command.matches(menuRegex)) {
+                nextMenu = menusIn.get(menuRegex);
             }
         }
-        if (nextCommand != null) {
-            nextCommand.run();
-        } else if (nextMenu != null) {
-            nextMenu.show();
-            nextMenu.execute();
-        }
-    }
-
-    public static Matcher getMatcher(String command, String regex) {
-        Pattern pattern = Pattern.compile(regex);
-        return pattern.matcher(command);
+        nextMenu.show();
+        nextMenu.execute();
     }
 }

9f88c53f1e5968d75b587711ad7c034c4e5638d0
Aryan Ahadinia <aryanahadinia24@gmail.com>
2020-04-14 21:06:56 +0430

    Add ManageUsersMenu

--- src/main/java/menu/profile/supervisorProfileMenu/ManageUsersMenu.java
+++ src/main/java/menu/profile/supervisorProfileMenu/ManageUsersMenu.java
@@ -1,4 +1,45 @@
 package menu.profile.supervisorProfileMenu;
 
-public class ManageUsersMenu {
+import menu.menuAbstract.Menu;
+
+public class ManageUsersMenu extends Menu {
+    public ManageUsersMenu(Menu parentMenu) {
+        super("Manage User Menu", parentMenu);
+
+        Menu View = new Menu("View", this) {
+            @Override
+            public void show() {
+            }
+
+            @Override
+            public void execute() {
+            }
+        };
+        menusIn.put("^view \\w+$", View);
+        menuForShow.add("View");
+
+        Menu DeleteUser = new Menu("Delete User", this) {
+            @Override
+            public void show() {
+            }
+
+            @Override
+            public void execute() {
+            }
+        };
+        menusIn.put("^delete user \\w+$", DeleteUser);
+        menuForShow.add("Delete User");
+
+        Menu CreateSupervisorProfile = new Menu("Create Supervisor Profile", this) {
+            @Override
+            public void show() {
+            }
+
+            @Override
+            public void execute() {
+            }
+        };
+        menusIn.put("^create manager profile \\w+$", CreateSupervisorProfile);
+        menuForShow.add("Create Manager Profile");
+    }
 }

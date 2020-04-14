5abe36bda2f224ddea784a768b82815212c3ead6
Aryan Ahadinia <aryanahadinia24@gmail.com>
2020-04-14 20:17:23 +0430

    Menu Added

---
+++ src/main/java/menu/ViewOrderMenuForCustomer.java
@@ -0,0 +1,34 @@
+package menu;
+
+public class ViewOrderMenuForCustomer extends Menu {
+    public ViewOrderMenuForCustomer(Menu parentMenu) {
+        super("View Order Menu", parentMenu);
+
+        Menu ShowOrderForCustomer = new Menu("Show Order", this) {
+            @Override
+            public void show() {
+
+            }
+
+            @Override
+            public void execute() {
+            }
+        };
+        menusIn.put("show order (\\w+)", ShowOrderForCustomer);
+        menuForShow.add("Show Order");
+
+        Menu Rate = new Menu("Rate", this) {
+            @Override
+            public void show() {
+            }
+
+            @Override
+            public void execute() {
+            }
+        };
+        menusIn.put("show order (\\w+)", Rate);
+        menuForShow.add("Rate");
+
+
+    }
+}

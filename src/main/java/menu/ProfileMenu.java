fc611d6cf4913a7e947eb7e9689940c30a903d10
Aryan Ahadinia <aryanahadinia24@gmail.com>
2020-04-14 18:24:07 +0430

    Menu Added

--- src/main/java/menu/ProfileMenu.java
+++ src/main/java/menu/ProfileMenu.java
@@ -1,28 +1,29 @@
 package menu;
 
+/**
+ * @author Aryan Ahadinia
+ * @since 0.0.1
+ */
+
 public abstract class ProfileMenu extends Menu {
     public ProfileMenu(String menuName, Menu parentMenu) {
         super(menuName, parentMenu);
         Menu viewPersonalInfo = new Menu("View Personal Info", this) {
             @Override
             public void show() {
-                super.show();
             }
 
             @Override
             public void execute() {
-                super.execute();
             }
         };
         Menu editPersonalInfo = new Menu("Edit", this) {
             @Override
             public void show() {
-                super.show();
             }
 
             @Override
             public void execute() {
-                super.execute();
             }
         };
     }

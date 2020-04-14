38acc3172dc5f45c1578417196ff85e6a52db107
Aryan Ahadinia <aryanahadinia24@gmail.com>
2020-04-14 21:19:31 +0430

    Add Menu

--- src/main/java/menu/profile/supervisorProfileMenu/ViewDiscountCodesMenu.java
+++ src/main/java/menu/profile/supervisorProfileMenu/ViewDiscountCodesMenu.java
@@ -1,6 +1,21 @@
 package menu.profile.supervisorProfileMenu;
 
-public class ViewDiscountCodesMenu {
-    public ViewDiscountCodesMenu() {
+import menu.menuAbstract.Menu;
+
+public class ViewDiscountCodesMenu extends Menu {
+    public ViewDiscountCodesMenu(Menu parentMenu) {
+        super("View Discount Codes Menu", parentMenu);
+
+        Menu ViewDiscountCode = new Menu("View Discount Code", this) {
+            @Override
+            public void show() {
+            }
+
+            @Override
+            public void execute() {
+            }
+        };
+        menusIn.put("^view discount code \\w+$", ViewDiscountCode);
+        menuForShow.add("View Discount Code");
     }
 }

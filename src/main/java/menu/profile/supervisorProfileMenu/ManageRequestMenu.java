9c54f3f70b69771b805cbf5c3f364073f53b49cd
Aryan Ahadinia <aryanahadinia24@gmail.com>
2020-04-14 21:36:14 +0430

    Add ManageRequestMenu

---
+++ src/main/java/menu/profile/supervisorProfileMenu/ManageRequestMenu.java
@@ -0,0 +1,33 @@
+package menu.profile.supervisorProfileMenu;
+
+import menu.menuAbstract.Menu;
+
+public class ManageRequestMenu extends Menu {
+    public ManageRequestMenu(Menu parentMenu) {
+        super("Manage Request Menu", parentMenu);
+
+        Menu AcceptRequest = new Menu("Accept Request", this) {
+            @Override
+            public void show() {
+            }
+
+            @Override
+            public void execute() {
+            }
+        };
+        menusIn.put("^accept (\\w+)$", AcceptRequest);
+        menuForShow.add("Accept");
+
+        Menu DeclineRequest = new Menu("Decline Request", this) {
+            @Override
+            public void show() {
+            }
+
+            @Override
+            public void execute() {
+            }
+        };
+        menusIn.put("^decline (\\w+)$", DeclineRequest);
+        menuForShow.add("Decline");
+    }
+}

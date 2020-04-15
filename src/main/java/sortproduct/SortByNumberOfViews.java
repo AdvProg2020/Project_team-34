b3c939c4d36302c0fd75cff0a61d8ca8c0558f78
rpirayadi <roozbehpirayadi@yahoo.com>
2020-04-15 12:03:27 +0430

    Update SortByNumberOfViews

--- src/main/java/sortproduct/SortByNumberOfViews.java
+++ src/main/java/sortproduct/SortByNumberOfViews.java
@@ -1,5 +1,12 @@
 package sortproduct;
 
-public class SortByNumberOfViews{
+import product.Product;
 
+import java.util.Comparator;
+
+public class SortByNumberOfViews implements Comparator<Product> {
+    @Override
+    public int compare(Product firstProduct, Product secondProduct) {
+        return firstProduct.getNumberOfViews() - secondProduct.getNumberOfViews();
+    }
 }

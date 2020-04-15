0915db4a02fafa919df6ed7ba44b7a3987d1d4f9
rpirayadi <roozbehpirayadi@yahoo.com>
2020-04-15 23:49:21 +0430

    Add createNewTable

--- src/main/java/database/ProductDateBase.java
+++ src/main/java/database/ProductDateBase.java
@@ -1,4 +1,24 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class ProductDateBase {
+    public static void createNewTable() {
+        String url = "jdbc:sqlite:.\\src\\main\\java\\recourses";
+
+        String sql = "CREATE TABLE IF NOT EXISTS Products (\n"
+                + "	id int,\n"
+                + "	name String \n"
+                + ");";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }

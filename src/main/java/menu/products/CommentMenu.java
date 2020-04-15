a876a58dead6cdd8e1aec8f13d9396071f138a82
s0heil20 <soheil.mh2000@gmail.com>
2020-04-15 16:12:23 +0330

    add CommentMenu

--- src/main/java/menu/products/CommentMenu.java
+++ src/main/java/menu/products/CommentMenu.java
@@ -5,5 +5,19 @@ import menu.menuAbstract.Menu;
 public class CommentMenu extends Menu {
     public CommentMenu(Menu parentMenu) {
         super("Comment Menu", parentMenu);
+
+        Menu addComment = new Menu("Comment Menu", this) {
+            @Override
+            public void show() {
+                super.show();
+            }
+
+            @Override
+            public void execute() {
+                super.execute();
+            }
+        };
+        menusIn.put("^Add comment$", addComment);
+        menuForShow.add("Add comment");
     }
 }

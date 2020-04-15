da315e08dc1fcfec46eb960ba2e073db97f714e9
s0heil20 <soheil.mh2000@gmail.com>
2020-04-15 16:19:37 +0330

    add DigestMenu

--- src/main/java/menu/products/DigestMenu.java
+++ src/main/java/menu/products/DigestMenu.java
@@ -5,5 +5,33 @@ import menu.menuAbstract.Menu;
 public class DigestMenu extends Menu {
     public DigestMenu(Menu parentMenu) {
         super("Digest Menu", parentMenu);
+
+        Menu addToCart = new Menu("Add To Cart", this) {
+            @Override
+            public void show() {
+                super.show();
+            }
+
+            @Override
+            public void execute() {
+                super.execute();
+            }
+        };
+        menusIn.put("^add to cart$", addToCart);
+        menuForShow.add("add to cart");
+
+        Menu selectSeller = new Menu("Select Seller", this) {
+            @Override
+            public void show() {
+                super.show();
+            }
+
+            @Override
+            public void execute() {
+                super.execute();
+            }
+        };
+        menusIn.put("^select seller ([^\\s]+)$", selectSeller);
+        menuForShow.add("select seller [seller_username]");
     }
 }

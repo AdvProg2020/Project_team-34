1f6d867fd5af0507cf4280e7802c0ada4558e92a
Aryan Ahadinia <aryanahadinia24@gmail.com>
2020-04-27 16:04:09 +0430

    Add Login/Logout/Profile to all menu

--- src/main/java/menu/menuAbstract/Menu.java
+++ src/main/java/menu/menuAbstract/Menu.java
@@ -16,12 +16,12 @@ import java.util.regex.Pattern;
 
 public abstract class Menu {
     public static Scanner scanner = new Scanner(System.in);
+    protected static Controller controller = new Controller();
     protected ArrayList<String> menuForShow;
     protected HashMap<String, Menu> menusIn;
     protected String command;
-    private String menuName;
     protected Menu parentMenu;
-    protected static Controller controller = new Controller();
+    private String menuName;
 
     public Menu(String menuName, Menu parentMenu) {
         this.menuName = menuName;
@@ -55,10 +55,10 @@ public abstract class Menu {
                 public void execute() {
                 }
             };
-            menusIn.put("^Sort$", SortCommands);
+            menusIn.put("^sort$", SortCommands);
             menuForShow.add("Sort");
 
-            if (this.parentMenu == null){
+            if (this.parentMenu == null) {
                 Menu exit = new Menu("Exit", this) {
                     @Override
                     public void show() {
@@ -70,11 +70,46 @@ public abstract class Menu {
                         System.exit(0);
                     }
                 };
-                menusIn.put("^Exit$", exit);
+                menusIn.put("^exit$", exit);
                 menuForShow.add("Exit");
-            }   else    {
-                menusIn.put("^Back$", parentMenu);
+            } else {
+                menusIn.put("^back$", parentMenu);
                 menuForShow.add("Back");
+            }
+
+            if (!controller.hasSomeOneLoggedIn()) {
+                if (!(menuName.equals("Login/Register Menu") || menuName.equals("Login") || menuName.equals("Create Account"))) {
+                    menusIn.put("^login/register$", new LoginMenu(this));
+                    menuForShow.add("Login/Register");
+                }
+            } else {
+                if (!menuName.equals("Logout Command")) {
+                    Menu LogoutCommand = new Menu("Logout Command", this) {
+                        @Override
+                        public void show() {
+                        }
+
+                        @Override
+                        public void execute() {
+                        }
+                    };
+                    menusIn.put("^logout$", LogoutCommand);
+                    menuForShow.add("Logout");
+                }
+
+                if (!(menuName.equals("Logout Command") || menuName.equals("Profile Viewer Terminal"))) {
+                    Menu ProfileViewerTerminal = new Menu("Profile Viewer Terminal", this) {
+                        @Override
+                        public void show() {
+                        }
+
+                        @Override
+                        public void execute() {
+                        }
+                    };
+                    menusIn.put("^profile$", ProfileViewerTerminal);
+                    menuForShow.add("Profile");
+                }
             }
         }
     }
@@ -89,30 +124,16 @@ public abstract class Menu {
         for (String menu : menuForShow) {
             System.out.println(menu);
         }
-        if (!controller.hasSomeOneLoggedIn()) {
-            System.out.println("Login/Register");
-        } else {
-            System.out.println("Logout");
-        }
     }
 
     public void execute() {
         command = scanner.nextLine();
-        Menu nextMenu = null;
+        Menu nextMenu = this;
         for (String menuRegex : menusIn.keySet()) {
             if (command.matches(menuRegex)) {
                 nextMenu = menusIn.get(menuRegex);
                 break;
             }
-        }
-        if (command.matches("Login/Register")) {
-            nextMenu = new LoginMenu(this);
-        } else if (command.matches("Logout")) {
-            //controller's function call
-            nextMenu = this;
-        }
-        if (nextMenu == null) {
-            nextMenu = this;
         }
         nextMenu.command = command;
         nextMenu.show();

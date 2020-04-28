6ce6cc4373cd555d5623b462559500475139d7f0
rpirayadi <roozbehpirayadi@yahoo.com>
2020-04-29 00:15:24 +0430

    Add constructor for database to Supplier

--- src/main/java/account/Supplier.java
+++ src/main/java/account/Supplier.java
@@ -12,9 +12,17 @@ public class Supplier extends Account{
     private SupplierLog supplierLog ;
 
 
-    public Supplier(String userName, String name, String familyName, String email, String phoneNumber, String password, int credit , String nameOfCompany) {
+    public Supplier(String userName, String name, String familyName, String email, String phoneNumber, String password,
+                    int credit , String nameOfCompany) {
         super(userName, name, familyName, email, phoneNumber, password, credit);
         this.nameOfCompany = nameOfCompany;
+    }
+
+    public Supplier(String userName, String name, String familyName, String email, String phoneNumber, String password,
+                    int credit, String nameOfCompany, SupplierLog supplierLog) {
+        super(userName, name, familyName, email, phoneNumber, password, credit);
+        this.nameOfCompany = nameOfCompany;
+        this.supplierLog = supplierLog;
     }
 
     public SupplierLog getSupplierLog() {

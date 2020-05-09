6eeb8c8f6f86f208befb2e7f37bd62e682a14a18
Aryan Ahadinia <aryanahadinia24@gmail.com>
2020-05-09 19:49:04 +0430

    add ProductInCartTest

---
+++ src/test/java/cart/ProductInCartTest.java
@@ -0,0 +1,22 @@
+package cart;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ProductInCartTest {
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+
+
+    }
+
+    @Test
+    public void name() {
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        
+    }
+}

80bc9b9799ee362963564048cd58a9df7498b5f3
Aryan Ahadinia <aryanahadinia24@gmail.com>
2020-06-07 23:37:02 +0430

    Develop GMenu (abstract)

--- src/main/java/gui/GMenu.java
+++ src/main/java/gui/GMenu.java
@@ -1,17 +1,24 @@
 package gui;
 
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import menu.menuAbstract.Menu;
 
 public abstract class GMenu {
-    public static final Node Header = createHeader();
-    protected Menu parentMenu;
-    private String menuName;
-    private Scene scene;
+    public static final Node HEADER = createHeader();
+    private final Menu parentMenu;
+    private final String menuName;
 
     public GMenu(String menuName, Menu parentMenu) {
         this.menuName = menuName;
         this.parentMenu = parentMenu;
     }
+
+    public Scene getScene() {
+        return createScene();
+    }
+
+    protected abstract Scene createScene();
 
     private static Node createHeader() {
         return null;
---
+++ src/main/java/gui/profile/ProfileGMenu.java
@@ -0,0 +1,17 @@
+package gui.profile;
+
+import gui.GMenu;
+import javafx.scene.Scene;
+import menu.menuAbstract.Menu;
+
+public class ProfileGMenu extends GMenu {
+
+    public ProfileGMenu(String menuName, Menu parentMenu) {
+        super(menuName, parentMenu);
+    }
+
+    @Override
+    protected Scene createScene() {
+        return null;
+    }
+}

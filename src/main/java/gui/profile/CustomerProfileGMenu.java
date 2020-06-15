5e965af588a298b6b3a243d9aa9fde05454e8dc7
rpirayadi <roozbehpirayadi@yahoo.com>
2020-06-15 14:10:46 +0430

    update CustomerProfileGMenu for second time

--- src/main/java/gui/profile/CustomerProfileGMenu.java
+++ src/main/java/gui/profile/CustomerProfileGMenu.java
@@ -5,7 +5,9 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -13,13 +15,17 @@ import java.io.File;
 
 public class CustomerProfileGMenu extends GMenu {
     private final GridPane backgroundLayout;
-    private final VBox mainPane;
+    private final HBox mainPane;
+    private final VBox buttonPane;
+    private final VBox viewPane;
 
 
     public CustomerProfileGMenu(String menuName, GMenu parentMenu, Stage stage) {
         super(menuName, parentMenu, stage);
         this.backgroundLayout = new GridPane();
-        this.mainPane = new VBox();
+        this.mainPane = new HBox();
+        this.buttonPane = new VBox();
+        this.viewPane = new VBox();
     }
 
     @Override
@@ -32,21 +38,30 @@ public class CustomerProfileGMenu extends GMenu {
         viewOrdersButton.getStylesheets().add(new File("src/main/resources/css/Style.css").toURI().toString());
         viewOrdersButton.getStyleClass().add("button");
 
-        Button viewBalanceButton = new Button("View Balance");
-        viewBalanceButton.getStylesheets().add(new File("src/main/resources/css/Style.css").toURI().toString());
-        viewBalanceButton.getStyleClass().add("button");
+//        Button viewBalanceButton = new Button("View Balance");
+//        viewBalanceButton.getStylesheets().add(new File("src/main/resources/css/Style.css").toURI().toString());
+//        viewBalanceButton.getStyleClass().add("button");
 
         Button viewDiscountCodesButton = new Button("View Discount Codes");
         viewDiscountCodesButton.getStylesheets().add(new File("src/main/resources/css/Style.css").toURI().toString());
         viewDiscountCodesButton.getStyleClass().add("button");
 
 
-        mainPane.setAlignment(Pos.CENTER);
+        buttonPane.setAlignment(Pos.CENTER);
+        buttonPane.setSpacing(10);
+        buttonPane.setPadding(new Insets(10, 10 , 10 , 10));
+        buttonPane.getChildren().addAll(viewCartButton, viewOrdersButton, viewDiscountCodesButton);
+
+        buttonPane.setStyle("-fx-background-color : #f8e8e2");
+
+        //int balance = controller.getAccountController().getAccountCredit();
+        Label balanceLabel = new Label(String.valueOf(450));
+
+        viewPane.getChildren().addAll(balanceLabel);
+
+        mainPane.getChildren().addAll(buttonPane, viewPane);
         mainPane.setSpacing(10);
         mainPane.setPadding(new Insets(10, 10 , 10 , 10));
-        mainPane.getChildren().addAll(viewCartButton, viewOrdersButton, viewBalanceButton, viewDiscountCodesButton);
-
-        mainPane.setStyle("-fx-background-color : #f8e8e2");
 
         backgroundLayout.getChildren().add(mainPane);
         backgroundLayout.setAlignment(Pos.CENTER);

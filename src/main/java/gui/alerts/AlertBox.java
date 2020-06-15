54d26799817330b4191d39197842cfd53715219b
Aryan Ahadinia <aryanahadinia24@gmail.com>
2020-06-15 15:03:19 +0430

    Complete AlertBox.java

--- src/main/java/gui/alerts/AlertBox.java
+++ src/main/java/gui/alerts/AlertBox.java
@@ -1,18 +1,35 @@
 package gui.alerts;
 
 import gui.GMenu;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-import menu.menuAbstract.Menu;
 
 public class AlertBox extends GMenu {
+    protected String alert;
+    protected String buttonText;
 
-    public AlertBox(String menuName, GMenu parentMenu, Stage stage) {
-        super(menuName, parentMenu, stage);
+    public AlertBox(GMenu parentMenu, Stage stage, String alert, String buttonText) {
+        super("Alert!", parentMenu, stage);
+        this.alert = alert;
+        this.buttonText = buttonText;
     }
 
     @Override
     protected Scene createScene() {
-        return null;
+        HBox hBox = new HBox();
+        Label alertLabel = new Label(alert);
+        Button okButton = new Button(buttonText);
+        hBox.getChildren().addAll(alertLabel, okButton);
+        hBox.setAlignment(Pos.CENTER);
+        GridPane background = new GridPane();
+        background.setAlignment(Pos.CENTER);
+        background.getChildren().addAll(hBox);
+        Scene scene = new Scene(background);
+        return scene;
     }
 }

a38d318fcf1ace2d1120ebc433617adda5a702aa
Aryan Ahadinia <aryanahadinia24@gmail.com>
2020-06-15 19:37:27 +0430

    Optimize Logs viewer and resolve duplicates

--- src/main/java/gui/profile/ViewLogsForCustomerGMenu.java
+++ src/main/java/gui/profile/ViewLogsForCustomerGMenu.java
@@ -2,9 +2,7 @@ package gui.profile;
 
 import gui.GMenu;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -17,26 +15,13 @@ public class ViewLogsForCustomerGMenu extends GMenu {
     @Override
     protected Scene createScene() {
         VBox logsBox = new VBox();
-        VBox mainLayout = new VBox();
-        GridPane background = new GridPane();
-        Scene scene = new Scene(background);
-
-        logsBox.setPadding(new Insets(10, 10, 10, 10));
-        logsBox.setSpacing(10);
         for (String log : controller.getAccountController().getCustomerLogs()) {
             logsBox.getChildren().add(customerLogBox(log));
         }
-
-        mainLayout.setAlignment(Pos.CENTER);
-        mainLayout.getChildren().addAll(createHeader(), logsBox);
-
-        background.getChildren().add(mainLayout);
-        background.setAlignment(Pos.CENTER);
-
-        return scene;
+        return createLogScene(logsBox);
     }
 
-    public VBox customerLogBox(String customerLog) {
+    private VBox customerLogBox(String customerLog) {
         VBox vBox = getLogBox();
         Text log = new Text(customerLog);
         vBox.getChildren().add(log);
--- src/main/java/gui/profile/ViewLogsForSupervisorGMenu.java
+++ src/main/java/gui/profile/ViewLogsForSupervisorGMenu.java
@@ -2,9 +2,7 @@ package gui.profile;
 
 import gui.GMenu;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -17,26 +15,13 @@ public class ViewLogsForSupervisorGMenu extends GMenu {
     @Override
     protected Scene createScene() {
         VBox logsBox = new VBox();
-        VBox mainLayout = new VBox();
-        GridPane background = new GridPane();
-        Scene scene = new Scene(background);
-
-        logsBox.setPadding(new Insets(10, 10, 10, 10));
-        logsBox.setSpacing(10);
         for (String log : controller.getAccountController().getSupervisorLogs()) {
             logsBox.getChildren().add(SupervisorLogBox(log));
         }
-
-        mainLayout.setAlignment(Pos.CENTER);
-        mainLayout.getChildren().addAll(createHeader(), logsBox);
-
-        background.getChildren().add(mainLayout);
-        background.setAlignment(Pos.CENTER);
-
-        return scene;
+        return createLogScene(logsBox);
     }
 
-    public VBox SupervisorLogBox(String supervisorLog) {
+    private VBox SupervisorLogBox(String supervisorLog) {
         VBox vBox = getLogBox();
         Text log = new Text(supervisorLog);
         vBox.getChildren().add(log);

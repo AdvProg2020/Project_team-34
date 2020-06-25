877a85c2f294fca43dbdf0061be70fe9c33327e2
Aryan Ahadinia <aryanahadinia24@gmail.com>
2020-06-25 16:04:40 +0430

    Add PeriodicCodedDiscount

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,6 +2,10 @@ package controller;
 
 import account.Account;
 import cart.Cart;
+import discount.CodedDiscount;
+
+import java.io.*;
+import java.util.Scanner;
 
 public class Controller {
     private Account account;
@@ -18,6 +22,31 @@ public class Controller {
         accountController = new AccountController(this);
         productController = new ProductController(this);
         offController = new OffController(this);
+        periodicCodedDiscount();
+    }
+
+    public void periodicCodedDiscount() {
+        try {
+            Scanner reader = new Scanner(new File("src/main/java/PeriodicCodedDiscount.txt"));
+            reader.nextLine();
+            String periodString = reader.nextLine();
+            String lastTimeString = reader.nextLine();
+            reader.close();
+            long period = Long.parseLong(periodString);
+            long lastTime = Long.parseLong(lastTimeString);
+            long timeNow = System.currentTimeMillis();
+            if (timeNow - lastTime >= period) {
+                FileWriter writer = new FileWriter(new File("src/main/java/PeriodicCodedDiscount.txt"));
+                writer.write("Data\n");
+                writer.write(periodString + "\n");
+                writer.write(String.valueOf(timeNow));
+                writer.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("PeriodicCodedDiscount Err");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public AccountController getAccountController() {

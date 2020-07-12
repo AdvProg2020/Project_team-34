b93f0cebdbb8dece7d7ff22e9c66889a1859e6dd
s0heil20 <soheil.mh2000@gmail.com>
2020-07-12 18:27:52 +0330

    add analyseRequest method

--- src/main/java/server/ClientThread.java
+++ src/main/java/server/ClientThread.java
@@ -1,17 +1,26 @@
 package server;
 
+import com.google.gson.*;
+import controller.Controller;
+import server.communications.ControllerSource;
+import server.communications.Request;
+
 import java.io.*;
+import java.lang.reflect.Method;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class ClientThread extends Thread {
     private final Server server;
     private final Socket socket;
     private final ObjectOutputStream objectOutputStream;
     private final ObjectInputStream objectInputStream;
+    private final Controller controller;
 
     public ClientThread(Server server, Socket socket) throws IOException {
         this.server = server;
         this.socket = socket;
+        this.controller = new Controller();
         this.objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
         this.objectInputStream = new ObjectInputStream(socket.getInputStream());
     }
@@ -19,5 +28,62 @@ public class ClientThread extends Thread {
     @Override
     public void run() {
         System.err.println("Thread run");
+    }
+
+    public void analyseRequest(JsonElement requestJson){
+
+        //Parsing the requestJson to Request!
+        Gson gson = new Gson();
+        Request request = gson.fromJson(requestJson,Request.class);
+        String functionName = request.getFunction();
+        String token = request.getToken();
+        //Analysing the token and checking if it's valid
+        ArrayList<Class> params = new ArrayList<>();
+        ArrayList<String> values = new ArrayList<>();
+        JsonParser jsonParser = new JsonParser();
+        JsonObject arguments = (JsonObject)jsonParser.parse(request.getInputs());
+        JsonArray argumentsArray = arguments.getAsJsonArray("Arguments");
+
+        if(argumentsArray != null){
+            for (JsonElement jsonElement : argumentsArray) {
+                values.add(jsonElement.getAsJsonObject().get("Value").getAsString());
+                try {
+                    params.add(Class.forName("java.lang.String"));
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        Method method;
+        try {
+            if (request.getSource() == ControllerSource.ACCOUNT_CONTROLLER) {
+                if (params.size() == 0) {
+                    method = controller.getAccount().getClass().getMethod(functionName);
+                } else {
+                    method = controller.getAccount().getClass().getMethod(functionName, params.toArray(new Class[params.size()]));
+                }
+                method.invoke(controller.getAccount(),values.toArray());
+            } else if (request.getSource() == ControllerSource.OFF_CONTROLLER) {
+                if (params.size() == 0) {
+                    method = controller.getAccount().getClass().getMethod(functionName);
+                } else {
+                    method = controller.getAccount().getClass().getMethod(functionName, params.toArray(new Class[params.size()]));
+                }
+                method.invoke(controller.getOffController(),values.toArray());
+            } else if (request.getSource() == ControllerSource.PRODUCT_CONTROLLER) {
+                if (params.size() == 0) {
+                    method = controller.getAccount().getClass().getMethod(functionName);
+                } else {
+                    method = controller.getAccount().getClass().getMethod(functionName, params.toArray(new Class[params.size()]));
+                }
+                method.invoke(controller.getProductController(),values.toArray());
+            }
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+            System.out.println("Can't call the method...!");
+        }
+
+
     }
 }

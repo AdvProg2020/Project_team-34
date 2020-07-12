5980f6e16579c9585731fb8271e16bc9d886d717
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2020-07-12 12:47:39 +0430

    Modify client-side Controller

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,58 +1,45 @@
 package controller;
 
-import account.Account;
-import cart.Cart;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Controller {
-    private Account account;
-    private Cart cart;
-    private boolean isFirstSupervisorCreated;
+    private Socket socket;
+    private ObjectInputStream objectInputStream;
+    private ObjectOutputStream objectOutputStream;
+
+    private String token;
+    private final boolean isFirstSupervisorCreated;
+
     private final AccountController accountController;
     private final ProductController productController;
     private final OffController offController;
 
     public Controller() {
-        account = null;
-        cart = new Cart(null);
-        isFirstSupervisorCreated = Account.isFirstSupervisorCreated();
-        accountController = new AccountController(this);
-        productController = new ProductController(this);
-        offController = new OffController(this);
-        periodicCodedDiscount();
-    }
-
-    public void periodicCodedDiscount() {
         try {
-            Scanner reader = new Scanner(new File("src/main/java/PeriodicCodedDiscount.txt"));
-            reader.nextLine();
-            int percent = Integer.parseInt(reader.nextLine());
-            int maxAmount = Integer.parseInt(reader.nextLine());
-            long period = Long.parseLong(reader.nextLine());
-            long lastTime = Long.parseLong(reader.nextLine());
-            long timeNow = System.currentTimeMillis();
-            reader.close();
-            if (timeNow - lastTime >= period) {
-                FileWriter writer = new FileWriter(new File("src/main/java/PeriodicCodedDiscount.txt"));
-                writer.write("Data\n");
-                writer.write(percent + "\n");
-                writer.write(maxAmount + "\n");
-                writer.write(period + "\n");
-                writer.write(timeNow + "\n");
-                writer.write("percent, max amount, period, lastTime");
-                writer.close();
-                accountController.controlCreateRandomCodesForCustomers(Account.getRandomCustomers(), percent, maxAmount);
+            this.socket = new Socket("localhost", 8080);
+            try {
+                this.objectInputStream = new ObjectInputStream(socket.getInputStream());
+                this.objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            } catch (IOException e) {
+                System.err.println("Error, IOStream can't connect");
+                System.exit(1);
             }
-        } catch (FileNotFoundException e) {
-            System.err.println("PeriodicCodedDiscount Err");
+        } catch (UnknownHostException e) {
+            System.err.println("Error, UnknownHostException");
+            System.exit(1);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Error, Socket Connection");
+            System.exit(1);
         }
+        this.isFirstSupervisorCreated = false;
+        this.token = null;
+        this.accountController = new AccountController(this);
+        this.productController = new ProductController(this);
+        this.offController = new OffController(this);
     }
 
     public AccountController getAccountController() {
@@ -67,27 +54,27 @@ public class Controller {
         return offController;
     }
 
-    public Account getAccount() {
-        return account;
-    }
-
-    public Cart getCart() {
-        return cart;
-    }
-
     public boolean getIsFirstSupervisorCreated() {
         return isFirstSupervisorCreated;
     }
 
-    public void setAccount(Account account) {
-        this.account = account;
+    public Socket getSocket() {
+        return socket;
     }
 
-    public void setCart(Cart cart) {
-        this.cart = cart;
+    public ObjectOutputStream getObjectOutputStream() {
+        return objectOutputStream;
     }
 
-    public void setIsFirstSupervisorCreated(boolean firstSupervisorCreated) {
-        isFirstSupervisorCreated = firstSupervisorCreated;
+    public ObjectInputStream getObjectInputStream() {
+        return objectInputStream;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 }

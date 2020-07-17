4ee63d07c667855fe110dec26d9de0acf8bb5da1
s0heil20 <soheil.mh2000@gmail.com>
2020-07-17 22:38:19 +0330

    ManageProductsSupplierGMenu/ManageRequestForSupplierGMenu no bug!

--- src/main/java/gui/profile/ManageRequestForSupplierGMenu.java
+++ src/main/java/gui/profile/ManageRequestForSupplierGMenu.java
@@ -88,10 +88,10 @@ public class ManageRequestForSupplierGMenu extends GMenu{
 
         try {
             for (Product product : controller.getAccountController().controlGetRequestForLoggedInSupplier()) {
-                products.getItems().add(Product.convertProductIdToRequestId(product.getProductId()));
+                products.getItems().add(controller.getProductController().convertProductIdToRequestId(product.getProductId()));
             }
             for (Sale sale : controller.getOffController().getAllSaleRequestsIdForThisSupplier()) {
-                products.getItems().add(Sale.convertSaleIdToRequestId(sale.getOffId()));
+                products.getItems().add(controller.getProductController().convertSaleIdToRequestId(sale.getOffId()));
             }
         }catch (ExceptionalMassage ex){
             new AlertBox(this, ex, controller).showAndWait();

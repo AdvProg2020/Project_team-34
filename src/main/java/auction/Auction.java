63081e4db240d3f9665e831d0696f1a6b499c80b
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2020-07-19 16:40:30 +0430

    Modify Auction

--- src/main/java/auction/Auction.java
+++ src/main/java/auction/Auction.java
@@ -2,13 +2,12 @@ package auction;
 
 import account.Customer;
 import account.Supplier;
+import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import exceptionalMassage.ExceptionalMassage;
-import log.CustomerLog;
+import communications.Utils;
 import product.Product;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Objects;
 
@@ -22,40 +21,105 @@ public class Auction {
     private Date end;
     private int wage;
 
+    private Auction(String json) {
+        JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+        this.identifier = jsonObject.get("identifier").getAsString();
+        this.chatRoomIdentifier = jsonObject.get("chatRoomIdentifier").getAsString();
+        this.product = Product.convertJsonStringToProduct(jsonObject.get("product").toString());
+        this.supplier = Supplier.convertJsonStringToSupplier(jsonObject.get("supplier").toString());
+        if (jsonObject.get("highestPromoter") instanceof JsonNull) {
+            this.highestPromoter = null;
+        } else {
+            this.highestPromoter = Customer.convertJsonStringToCustomer(jsonObject.get("highestPromoter").toString());
+        }
+        if (jsonObject.get("highestPromotion") instanceof JsonNull) {
+            this.highestPromotion = null;
+        } else {
+            this.highestPromotion = Integer.parseInt(jsonObject.get("highestPromotion").toString());
+        }
+        this.end = new Date(Long.parseLong(jsonObject.get("end").getAsString()));
+    }
+
     public String getIdentifier() {
         return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
     }
 
     public String getChatRoomIdentifier() {
         return chatRoomIdentifier;
     }
 
+    public void setChatRoomIdentifier(String chatRoomIdentifier) {
+        this.chatRoomIdentifier = chatRoomIdentifier;
+    }
+
     public Product getProduct() {
         return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
     }
 
     public Supplier getSupplier() {
         return supplier;
     }
 
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
     public Customer getHighestPromoter() {
         return highestPromoter;
+    }
+
+    public void setHighestPromoter(Customer highestPromoter) {
+        this.highestPromoter = highestPromoter;
     }
 
     public Integer getHighestPromotion() {
         return highestPromotion;
     }
 
+    public void setHighestPromotion(Integer highestPromotion) {
+        this.highestPromotion = highestPromotion;
+    }
+
     public Date getEnd() {
         return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
     }
 
     public int getWage() {
         return wage;
     }
 
-    public Auction convertJsonStringToAuction(String json) {
-        return null;
+    public void setWage(int wage) {
+        this.wage = wage;
+    }
+
+    public String toJson() {
+        JsonObject jsonObject = new JsonObject();
+        JsonParser jsonParser = new JsonParser();
+        jsonObject.add("identifier", jsonParser.parse(Utils.convertObjectToJsonString(identifier)));
+        jsonObject.add("chatRoomIdentifier", jsonParser.parse(Utils.convertObjectToJsonString(chatRoomIdentifier)));
+        jsonObject.add("product", jsonParser.parse(Utils.convertObjectToJsonString(product)));
+        jsonObject.add("supplier", jsonParser.parse(Utils.convertObjectToJsonString(supplier)));
+        jsonObject.add("highestPromoter", jsonParser.parse(Utils.convertObjectToJsonString(highestPromoter)));
+        jsonObject.add("highestPromotion", jsonParser.parse(Utils.convertObjectToJsonString(String.valueOf(highestPromotion))));
+        jsonObject.add("end", jsonParser.parse(Utils.convertObjectToJsonString(String.valueOf(end.getTime()))));
+        jsonObject.add("wage", jsonParser.parse(Utils.convertObjectToJsonString(String.valueOf(wage))));
+        return jsonObject.toString();
+    }
+
+    public static Auction convertJsonStringToAuction(String json) {
+        return new Auction(json);
     }
 
     @Override

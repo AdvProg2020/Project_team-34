49ca8545960070740938696ef3830c8145d2d0ad
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2020-07-21 15:40:27 +0430

    Modify WalletGMenu

--- src/main/java/gui/paymentMenu/WalletGMenu.java
+++ src/main/java/gui/paymentMenu/WalletGMenu.java
@@ -11,7 +11,6 @@ import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import org.w3c.dom.Text;
 
 public class WalletGMenu extends GMenu {
     public WalletGMenu(GMenu parentMenu, Stage stage, Controller controller) {

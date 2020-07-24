38b8baf3215abf12b4f101bbf32b4098fa327be5
rpirayadi <roozbehpirayadi@yahoo.com>
2020-07-24 14:07:59 +0430

    changes to try to encrypt

---
+++ src/main/java/peer/GetPublicKeyHandler.java
@@ -0,0 +1,18 @@
+package peer;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.security.PublicKey;
+
+//public class GetPublicKeyHandler implements Runnable {
+//
+//    @Override
+//    public void run() {
+//        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+//        ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+//        System.err.println("==================");
+//        Object object = objectInputStream.readObject();
+//        System.err.println("}}}}}}}}}}}}}}}}}}}");
+//        PublicKey publicKey = (PublicKey) object;
+//    }
+//}
--- src/main/java/peer/PeerNode.java
+++ src/main/java/peer/PeerNode.java
@@ -55,18 +55,15 @@ public class PeerNode
         }
     }
 
-    public void sendRequest(String filePath, String host, int port) throws Exception
-    {
+    public void sendRequest(String filePath, String host, int port) throws Exception {
         System.out.println("trying to connect to " + port);
         Socket socket = new Socket(host, port);
+
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-
         objectOutputStream.writeObject(asymmetric.getPublicKey());
-        objectOutputStream.close();
+        objectOutputStream.flush();
 
-        PrintWriter printWriter = new PrintWriter( socket.getOutputStream(), true );
-        printWriter.println(filePath);
-
+        objectOutputStream.writeObject(filePath);
 
 
         InputStream inputStream = socket.getInputStream();
@@ -76,27 +73,35 @@ public class PeerNode
         System.out.println(response);
 
         if(response.equals("Successful")) {
+            System.out.println("gand");
             String[] data = filePath.split("\\\\");
             String myFilePath = ".\\src\\main\\resources\\download\\" + data[data.length-1 ];
 
-            byte[] buffer = new byte[1024];
-            byte[] decrypted = new byte[1024];
+            byte[] buffer = new byte[256];
+            byte[] decrypted ;
             int bytes = 0;
 
             FileOutputStream fileOutputStream = new FileOutputStream(new File(myFilePath));
 
-            while ((bytes = inputStream.read(buffer)) > 0 ) {
+            while ((bytes = inputStream.read(buffer, 0, bytes)) > 0 ) {
+                System.out.println(bytes);
+                System.out.println("[[");
                 System.out.println(new String(buffer));
+//                inputStream.read(buffer, 0 , bytes);
+                System.out.println(new String(buffer));
+                System.out.println("[[");
                 decrypted = asymmetric.do_RSADecryption(buffer);
                 System.out.println(new String(decrypted));
-                fileOutputStream.write(decrypted, 0, bytes);
+                fileOutputStream.write(decrypted, 0, 256);
             }
 
             fileOutputStream.close();
         }
-        printWriter.close();
+        bufferedReader.close();
         socket.close();
-    }
+//
+
+        }
     public static ArrayList<PeerNode> getContacts() {
         return contacts;
     }

669793ba10ff0b512a881ebf04094982422d0192
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2020-07-24 13:57:19 +0430

    Modify Auction

--- src/main/java/auction/Auction.java
+++ src/main/java/auction/Auction.java
@@ -197,11 +197,13 @@ public class Auction {
     }
 
     public void end() {
-        if (highestPromoter != null) {
-            try {
-                new CustomerLog(this, wage);
-            } catch (ExceptionalMassage exceptionalMassage) {
-                System.err.println("Couldn't add log.");
+        if (isEnded) {
+            if (highestPromoter != null) {
+                try {
+                    new CustomerLog(this, wage);
+                } catch (ExceptionalMassage exceptionalMassage) {
+                    System.err.println("Couldn't add log.");
+                }
             }
         }
         //product out

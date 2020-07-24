2aeb511a73e797dbc36645b22e669fd38feebf78
rpirayadi <roozbehpirayadi@yahoo.com>
2020-07-24 12:18:51 +0430

    use Validation in AccountController

--- src/main/java/server/security/Validation.java
+++ src/main/java/server/security/Validation.java
@@ -8,8 +8,10 @@ import java.util.regex.Pattern;
 
 public class Validation {
 
-    public static final int STRING_MAX_LENGTH = 15;
+    public static final int STRING_MAX_LENGTH = 20;
     public static final int EMAIL_MAX_LENGTH = 40;
+    public static final int ADDRESS_MAX_LENGTH = 150;
+    public static final int MESSAGE_MAX_LENGTH = 250;
 
     public static void normalStringValidation(String input) throws ExceptionalMassage {
         if(input.length() > STRING_MAX_LENGTH){
@@ -70,6 +72,64 @@ public class Validation {
             throw new ExceptionalMassage("Not a boolean");
         }
     }
+
+    public static void identifierValidation(String input)throws  ExceptionalMassage{
+        if(!getMatcher(input, "T34(CR|AC|CA|PC|SI|SA|CO|SC|PR)\\d{15}").matches())
+            throw new ExceptionalMassage("Invalid identifier");
+    }
+
+    public static void addressValidation(String input)throws ExceptionalMassage{
+        if(input.length() > ADDRESS_MAX_LENGTH){
+            throw new ExceptionalMassage("You have Exceeded maxLength");
+        }
+    }
+
+    public static void postalCodeValidation(String input)throws ExceptionalMassage{
+        if(!getMatcher(input,"\\d{10}").matches())
+            throw new ExceptionalMassage("Invalid Postal Code");
+        if(!getMatcher(input, "^\\w+$").matches()){
+            throw new ExceptionalMassage("Invalid String");
+        }
+    }
+
+    public static void discountCodeValidation(String input)throws ExceptionalMassage{
+        if(input.length() > STRING_MAX_LENGTH){
+            throw new ExceptionalMassage("You have Exceeded maxLength");
+        }
+        if(!getMatcher(input, "^\\w+$").matches()){
+            throw new ExceptionalMassage("Invalid Coded Discount");
+        }
+    }
+
+    public static void messageValidation(String input)throws ExceptionalMassage{
+        if(input.length() > MESSAGE_MAX_LENGTH){
+            throw new ExceptionalMassage("You have Exceeded maxLength");
+        }
+    }
+
+    public static void percentValidation(String input) throws ExceptionalMassage{
+        int number;
+        try{
+            number = Integer.parseInt(input);
+        }catch (NumberFormatException exception){
+            throw new ExceptionalMassage("Not an Integer");
+        }
+        if(number < 0 || number >100){
+            throw new ExceptionalMassage("Enter a Number Between 0 and 100");
+        }
+    }
+
+    public static void accountNumberValidation(String input)throws ExceptionalMassage{
+        int number;
+        try{
+            number = Integer.parseInt(input);
+        }catch (NumberFormatException exception){
+            throw new ExceptionalMassage("Not a Valid Account Number");
+        }
+        if(!getMatcher(input, "\\d{6}").matches())
+            throw new ExceptionalMassage("Not a Valid Account Number");
+    }
+
 
     public static Matcher getMatcher(String input , String regexString){
         Pattern pattern = Pattern.compile(regexString);
